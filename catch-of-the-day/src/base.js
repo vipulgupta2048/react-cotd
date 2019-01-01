a289a2ab14be866b15cb7849820a0919e667cbb0
Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>
2019-01-01 13:06:20 +0530

    Commit for GitHub

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyB3FJkGsAl0QlQNh4PVYpqaeoCkMXsDWzw",
-  authDomain: "catch-of-the-day-vipul-2048.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-vipul-2048.firebaseio.com"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: ""
 });
 
 const base = Rebase.createClass(firebaseApp.database());
