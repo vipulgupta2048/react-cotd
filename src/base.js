b529d810757b8b416499168436ad55a4560c3ca3
Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>
2019-02-04 21:53:02 +0530

    Changes to BASE

--- src/base.js
+++ src/base.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/app";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyB3FJkGsAl0QlQNh4PVYpqaeoCkMXsDWzw",
-  authDomain: "catch-of-the-day-vipul-2048.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-vipul-2048.firebaseio.com",
-  projectId: "catch-of-the-day-vipul-2048",
-  storageBucket: "catch-of-the-day-vipul-2048.appspot.com",
-  messagingSenderId: "133696293648"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 });
 
 export const auth = firebaseApp.auth();
